a9a973dad717af30b154c4a74be54e1412bc4170
Ariana <arigarcia326@knights.ucf.edu>
2023-05-02 01:31:18 +0000

    May 1, 2023, 9:31 PM

--- src/components/Matching/Matching.js
+++ src/components/Matching/Matching.js
@@ -72,7 +72,7 @@ export default function Matching({ route }) {
                 background: selectedIndex === null ? 'white' 
                 : selectedIndex === index ? `${matchingColors[selectedIndex]}` 
                 : matchedItems.some(i => i.index === index) === false? 'white' 
-                : matchingColors[matchedItems.filter(i => i.index === index).index],
+                : matchingColors[matchedItems.find(i => i.index === index).index],
                 // backgroundColor:
                 //   selectedOption === option.option ? 'yellow' : matchedItems.find(
                 //       (matchedItem) =>
